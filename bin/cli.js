8a3364854a9d4364e0298d8445a1fa82b8f44c5d
Ani Channarasappa <ani@locust.dev>
2019-01-20 18:12:32 -0500

    Expanded cli

--- bin/cli.js
+++ bin/cli.js
@@ -41,15 +41,10 @@ yargs
   .command('start', 'starts a job and crawls until a stop condition is met', (yargs) => {
   
     return yargs
-      .option('follow-links', {
-        describe: 'Start a full local crawl and follow links until a stop condition is met using redis and chrome',
-        default: false,
-      })
       .option('bootstrap', {
         describe: 'Start redis and browserless Docker containers if not already available',
         default: false,
       })
-      .alias('f', 'follow-links')
       .alias('b', 'bootstrap')
       .demandCommand(1, 'A file path to a job file is required')
       .usage('cli run <path_to_file>')
