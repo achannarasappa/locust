133f1ff530cc72ae6b9d42ab31fbe560a078ebc6
Ani Channarasappa <ani@locust.dev>
2019-06-08 14:49:01 -0400

    Added dev scrape target server

---
+++ test/server/server.js
@@ -0,0 +1,59 @@
+const Hapi = require('@hapi/hapi');
+const Vision = require('@hapi/vision');
+const { readFileSync } = require('fs');
+
+const MAX_EXTRA_LATENCY = 200;
+const MIN_EXTRA_LATENCY = 100;
+
+const addRoutes = async (server) => {
+
+  await server.register(Vision);
+
+  server.views({
+    engines: { html: require('handlebars') },
+    path: __dirname + '/templates',
+  });
+
+  JSON.parse(readFileSync(__dirname + '/pages.json'))
+  .map(({ title, links, depth, path }) => {
+
+    server.route({
+      method: 'GET',
+      path,
+      handler: (r, h) => new Promise((resolve, reject) => {
+        
+        setTimeout(() => resolve(h.view('page', {
+          title,
+          links,
+          depth,
+        })), Math.floor(Math.random() * (MAX_EXTRA_LATENCY - MIN_EXTRA_LATENCY + 1) + MIN_EXTRA_LATENCY))
+
+        
+
+      })
+    });
+
+  });
+
+};
+
+const init = async () => {
+
+  const server = Hapi.server({
+      port: 3001,
+      host: 'localhost'
+  });
+
+  await addRoutes(server);
+
+  await server.start();
+  console.log('Test server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+  console.log(err);
+  process.exit(1);
+});
+
+init();
---
+++ test/server/site-generator.js
@@ -0,0 +1,47 @@
+/**
+ * site-generator
+ * 
+ * Utility script to generation site.json which defines the structure of the site used for 
+ * functional testing purposes. site.json should relatively static since there will be 
+ * assertions made against the specific structure.
+ */
+
+const dockerNames = require('docker-names');
+const { writeFileSync } = require('fs');
+
+const MAX_DEPTH = 6;
+const MIN_LINKS = 1;
+const MAX_LINKS = 5;
+
+const randomInt = () => Math.floor(Math.random() * (MAX_LINKS - MIN_LINKS + 1) + MIN_LINKS);
+
+const generatePage = (title, subPageCount, parentPath, depth = 0) => {
+
+  const path = depth === 0 ? '' : [ parentPath, title ].join('/');
+  const subPageTitles = Array(subPageCount)
+  .fill(0)
+  .map(() => dockerNames.getRandomName());
+
+  return Array.prototype.concat.apply([], [
+    {
+      title,
+      depth,
+      path: depth === 0 ? '/' : path,
+      links: subPageTitles.map((title) => [ path, title ].join('/')),
+    },
+    ...subPageTitles
+      .map((title) => generatePage(
+        title,
+        MAX_DEPTH > depth + 1
+          ? randomInt()
+          : 0,
+        path,
+        depth + 1,
+      ))
+    ]);
+
+};
+
+const pages = JSON.stringify(generatePage('home', randomInt()));
+
+writeFileSync(`${__dirname}/pages.json`, pages);
